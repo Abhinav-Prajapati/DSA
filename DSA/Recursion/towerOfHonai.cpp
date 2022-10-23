d6cf778e1d0f8f2bfc263f536c27a04c3649dd60
Abhinav <abhinavprajapati2001@gmail.com>
2022-10-23 21:30:40 +0530

    added tower of honai problem

---
+++ DSA/Recursion/towerOfHonai.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void toh(int disc_number, char from, char buffer, char to)
+{
+    if (disc_number == 1)
+    {
+        cout << from << " to " << to<<endl;
+    }
+    else
+    {
+        toh(disc_number - 1, from, to, buffer);
+
+        cout << from << " to " << to<<endl;
+
+        toh(disc_number - 1, buffer, from, to);
+    }
+}
+int main()
+{
+
+    toh(3, 'a', 'b', 'c');
+    return 0;
+}
