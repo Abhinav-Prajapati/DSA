fd82bafb99650618b6267652cd559df4d5aea1da
Abhinav <abhinavprajapati2001@gmail.com>
2022-10-23 12:04:47 +0530

    added ncr and fibonachi seires

---
+++ DSA/Recursion/fibonachi.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int fibo(int n)
+{
+    if (n <= 1)
+        return n;
+    return fibo(n - 2) + fibo(n - 1);
+}
+int fibo_hashmap(int n,int arr[])
+{
+if(arr[n]==-1) return n;
+else{
+    if(arr[n-2]==-1) arr[n-2]=fibo_hashmap(n-2,arr);
+    if(arr[n-1]==-1) arr[n-1]=fibo_hashmap(n-1,arr);
+
+}
+return arr[n-2]+arr[n-1];
+}
+void printArray(int arr[], int len)
+{
+    cout << "Printing array elements , Size = " << len << endl;
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i] << ",";
+    }
+}
+
+int main()
+{
+    int arr[10];
+    for (int i = 0; i <10;i++)
+    {
+        arr[i] = -1;
+    }
+    // cout << fibo(6);
+    cout<<fibo_hashmap(6,arr);
+    
+    return 0;
+}
---
+++ DSA/Recursion/ncr_pascal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int ncr_pascle(int n, int r)
+{
+    if (n == r || r == 0)
+        return 1;
+    return ncr_pascle(n - 1, r - 1) + ncr_pascle(n - 1, r);
+}
+int main()
+{
+cout<<ncr_pascle(4,2);
+    return 0;
+}
